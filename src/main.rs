fffe90b0dc51a96e90d7dd28c53e954fa8d5dd17
Alexey Gerasev <alexey.gerasev@gmail.com>
2016-10-13 01:43:12 +0700

    measure fps

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,43 @@
-#[macro_use(color)]
 extern crate mm;
-use mm::Color;
 
+struct FPSCounter<'a> {
+	m: &'a mm::System,
+	start: u32,
+	frames: u32,
+	pub fps: f64,
+}
+
+impl<'a> FPSCounter<'a> {
+	pub fn new(m: &mm::System) -> FPSCounter {
+		FPSCounter { m: m, start: m.get_ticks(), frames: 0, fps: 0.0 }
+	}
+	pub fn count(&mut self) {
+		self.frames += 1;
+		let ticks = self.m.get_ticks();
+		let diff = ticks - self.start;
+		if diff > 1000 {
+			self.fps = (self.frames as f64) * 1000.0/(diff as f64);
+			self.frames = 0;
+			self.start += (diff/1000)*1000;
+			println!("fps: {:.2}", self.fps);
+		}
+	}
+}
 
 fn main() {
 	let mut i = 0;
-	mm::init(800, 600);
-	while mm::handle() == 0 {
-		let r = (1 + i%100) as f32;
-		let g = 100 as f32;
-		let b = (100 - i%100) as f32;
-		mm::set_color(color!(0.01*r, 0.01*g, 0.01*b));
-		let (w,h) = mm::get_size();
+	let m = mm::System::new(800, 600).unwrap();
+	let mut fpscnt = FPSCounter::new(&m);
+	while m.handle() == 0 {
+		let (w,h) = m.get_size();
 		for ix in 0..w {
 			for iy in 0..h {
-				mm::draw_point(ix, iy);
+				m.draw_pixel(ix, iy, i | (0xff << 3*8));
 			}
 		}
-		mm::update();
-		// mm::sleep(40);
+		m.update();
+		// m.sleep(40);
+		fpscnt.count();
 		i += 1;
 	}
-	mm::quit();
 }
